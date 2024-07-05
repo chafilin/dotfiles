19b0a2425a18e72d4464ddf89eb5f3b2db6d33f6
Schedrin Vladimir <chafiliny@gmail.com>
2024-07-06 05:19:26 +0600

    add pnpm in installation

--- scripts/langs.sh
+++ scripts/langs.sh
@@ -5,6 +5,13 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 fi
 
+echo "Installing pnpm"
+read -p "Do you want to install pnpm? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  brew install pnpm
+fi
+
 echo "Installing rbenv"
 read -p "Do you want to install rbenv? (y/n) " -n 1 -r
 echo
