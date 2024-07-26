5464eb2bec3a1d3b8d2df5defc86952688244925
Schedrin Vladimir <chafiliny@gmail.com>
2024-07-26 13:35:22 +0600

    Update tmux, kitty and nvim settings

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "<C-Down>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Up>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
