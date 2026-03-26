1be319aa0c5f0f076306d71e057f519639c4e123
Shchedrin Vladimir <chafiliny@gmail.com>
2026-03-26 11:30:44 +0100

    feature(nvim): change eslint and prettier to oxlint and biome

--- nvim/.config/nvim/lua/plugins/frontend.lua
+++ nvim/.config/nvim/lua/plugins/frontend.lua
@@ -27,4 +27,62 @@ return {
       },
     },
   },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local biome_filetypes = {
+        "css",
+        "graphql",
+        "json",
+        "jsonc",
+      }
+      local biome_import_filetypes = {
+        "astro",
+        "javascript",
+        "javascriptreact",
+        "svelte",
+        "typescript",
+        "typescriptreact",
+        "vue",
+      }
+
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters = opts.formatters or {}
+      opts.formatters["biome-organize-imports"] = { require_cwd = true }
+
+      for _, ft in ipairs(biome_filetypes) do
+        opts.formatters_by_ft[ft] = { "biome" }
+      end
+
+      for _, ft in ipairs(biome_import_filetypes) do
+        opts.formatters_by_ft[ft] = { "oxlint", "biome-organize-imports", "biome" }
+      end
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        astro = { "oxlint" },
+        javascript = { "oxlint" },
+        javascriptreact = { "oxlint" },
+        svelte = { "oxlint" },
+        typescript = { "oxlint" },
+        typescriptreact = { "oxlint" },
+        vue = { "oxlint" },
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+
+      if not vim.tbl_contains(opts.ensure_installed, "oxlint") then
+        table.insert(opts.ensure_installed, "oxlint")
+      end
+    end,
+  },
 }
